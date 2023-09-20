3a85952545992d7d04165e05e8225ad24b757822
nyulnyul <nyul0223@naver.com>
2023-09-20 19:34:46 +0900

    프로젝트 생성
    - 프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ITC_Football"
+include(":app")
+ 
