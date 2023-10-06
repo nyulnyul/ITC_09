c3cdeb04d333fed3864c95c826ec9a6edc53bc2e
nyulnyul <nyul0223@naver.com>
2023-10-06 11:13:13 +0900

    feat(mypage):내 정보 불러오기
    - 내 정보 불러오기

--- app/src/main/java/com/example/itc_football/view/MyPageActivity.kt
+++ app/src/main/java/com/example/itc_football/view/MyPageActivity.kt
@@ -1,10 +1,14 @@
 package com.example.itc_football.view
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.itc_football.R
 import com.example.itc_football.databinding.MyPageActivityBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MyPageActivity : AppCompatActivity() {
     private lateinit var binding: MyPageActivityBinding
@@ -16,6 +20,39 @@ class MyPageActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            val uid = user.uid
+            val db = FirebaseFirestore.getInstance()
+            val docRef = db.collection("users").document(uid)
+            docRef.get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val name = document.getString("name")
+                        val dept = document.getString("dept")
+                        val email = document.getString("email")
+
+
+                        binding.myname.text = name
+
+                        binding.mydept.text = dept
+                        if (email != null) {
+                            binding.mynum.text = email.split("@")[0]
+                        }
+
+                    } else {
+                        Log.d(TAG, "No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "get failed with ", exception)
+                }
+        } ?: run {
+            // No user is signed in
+        }
+
+
+
         binding.bottomNavigation.selectedItemId = binding.bottomNavigation.menu.getItem(2).itemId
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -24,16 +61,19 @@ class MyPageActivity : AppCompatActivity() {
                     startActivity(intent)
                     true
                 }
+
                 R.id.bottom_chat -> {
                     val intent = Intent(this, ChatListActivity::class.java)
                     startActivity(intent)
                     true
                 }
+
                 R.id.bottom_mypage -> {
                     val intent = Intent(this, MyPageActivity::class.java)
                     startActivity(intent)
                     true
                 }
+
                 else -> false
             }
         }
