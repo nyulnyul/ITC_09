4671b94478dccec4ff90ce8feb1ac3f6e71b79be
SuuuHyeon <tngutnqls6909@gmail.com>
2023-10-06 11:11:34 +0900

    feat(recyclerView): 리스트뷰 -> 리사이클러뷰 변경, 어댑터 구조 변경
    - 리스르뷰에서 리사이클뷰로 레이아웃 변경
    - Product 및 어댑터 구조변경

--- app/src/main/java/com/example/itc_football/ProductAdapter.kt
+++ app/src/main/java/com/example/itc_football/ProductAdapter.kt
@@ -1,47 +1,78 @@
 package com.example.itc_football
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.itc_football.view.ItemListActivity
 import com.google.android.material.imageview.ShapeableImageView
 
-class ProductAdapter (val context: Context, val ProductList: ArrayList<Product>): BaseAdapter() {
-    override fun getCount(): Int {
-        return ProductList.size
+class ProductAdapter(private val productList : ArrayList<Product>): RecyclerView.Adapter<ProductAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductAdapter.MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_activity, parent, false)
+        return MyViewHolder(itemView)
     }
 
-    override fun getItem(position: Int): Any {
-        return ProductList[position]
-    }
+    override fun onBindViewHolder(holder: ProductAdapter.MyViewHolder, position: Int) {
 
-    override fun getItemId(position: Int): Long {
-        return 0
-    }
+        val currentItem = productList[position]
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.item_activity, null)
-
-        val imageUrl = view.findViewById<ShapeableImageView>(R.id.imageUrl)
-        val productName = view.findViewById<TextView>(R.id.productName)
-        val productPrice = view.findViewById<TextView>(R.id.productPrice)
-        val maxMember = view.findViewById<TextView>(R.id.maxMember)
-        val nowMember = view.findViewById<TextView>(R.id.nowMember)
-
-        val product = ProductList[position]
-
-        productName.text = product.productName
-        productPrice.text = product.productPrice.toString()
-        maxMember.text = product.maxMember.toString()
-        nowMember.text = product.nowMember.toString()
+        holder.productName.text = currentItem.productName
+        holder.productPrice.text = currentItem.productPrice.toString()
+        holder.maxMember.text = currentItem.maxMember.toString()
+        holder.nowMember.text = currentItem.nowMember.toString()
         // Glide를 이용하여 ImageView에 url 이미지를 세팅
-        Glide.with(context).load(product.imageUrl).into(imageUrl)
-
-        return view
+        Glide.with(holder.itemView.context).load(currentItem.imageUrl).into(holder.imageUrl)
     }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageUrl: ShapeableImageView = itemView.findViewById(R.id.imageUrl)
+        val productName: TextView = itemView.findViewById(R.id.productName)
+        val productPrice: TextView = itemView.findViewById(R.id.productPrice)
+        val maxMember: TextView = itemView.findViewById(R.id.maxMember)
+        val nowMember: TextView = itemView.findViewById(R.id.nowMember)
+    }
+
+    //    override fun getCount(): Int {
+//        return ProductList.size
+//    }
+//
+//    override fun getItem(position: Int): Any {
+//        return ProductList[position]
+//    }
+//
+//    override fun getItemId(position: Int): Long {
+//        return 0
+//    }
+//
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        val view: View = LayoutInflater.from(context).inflate(R.layout.item_activity, null)
+//
+//        val imageUrl = view.findViewById<ShapeableImageView>(R.id.imageUrl)
+//        val productName = view.findViewById<TextView>(R.id.productName)
+//        val productPrice = view.findViewById<TextView>(R.id.productPrice)
+//        val maxMember = view.findViewById<TextView>(R.id.maxMember)
+//        val nowMember = view.findViewById<TextView>(R.id.nowMember)
+//
+//        val product = ProductList[position]
+//
+//        productName.text = product.productName
+//        productPrice.text = product.productPrice.toString()
+//        maxMember.text = product.maxMember.toString()
+//        nowMember.text = product.nowMember.toString()
+//        // Glide를 이용하여 ImageView에 url 이미지를 세팅
+//        Glide.with(context).load(product.imageUrl).into(imageUrl)
+//
+//        return view
+//    }
 
 }
